61c9029b7302eb784b59ba25a436e97af46879ec
vinniciusgomes <vinnicius.brito@iclubs.tech>
2022-03-29 16:35:48 -0300

    Atualizando react

--- demo/App.tsx
+++ demo/App.tsx
@@ -7,7 +7,6 @@ import { Column } from './styled'
 export const App: FC = () => {
   return (
     <Column>
-      <h1>Create your demo app</h1>
       <MyComponent>
         <span>Teste</span>
       </MyComponent>
